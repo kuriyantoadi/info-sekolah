e991359ae16a091c2319ee3aa9a3985090b14ad2
kuriyantoadi <melatikuning212@gmail.com>
2022-02-13 09:36:31 +0700

    update 13 feb 2022, update 4

--- view/angkatan.php
+++ view/angkatan.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-md-12 mb-4">
             <center>
-                <h3 style="margin-top:  25px;"><b>Daftar Siswa Kelas X</b></h3>
+                <h3 style="margin-top:  25px;"><b>Daftar Siswa</b></h3>
             </center>
         </div>
 
